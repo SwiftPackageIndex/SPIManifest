fb6f0e002550f90a19268454560010de5f85ca6c
Sven A. Schmidt <sas@finestructure.co>
2022-06-28 11:13:24 +0200

    Drop SwiftVersion.shortVersion

--- Sources/SPIManifest/SwiftVersion.swift
+++ Sources/SPIManifest/SwiftVersion.swift
@@ -1,24 +1,11 @@
-public enum SwiftVersion: String, Codable {
+public typealias ShortVersion = String
+
+
+public enum SwiftVersion: ShortVersion, Codable {
     case v5_3 = "5.3"
     case v5_4 = "5.4"
     case v5_5 = "5.5"
     case v5_6 = "5.6"
-}
-
-
-extension SwiftVersion {
-    public var shortVersion: String {
-        switch self {
-            case .v5_3:
-                return "5.3"
-            case .v5_4:
-                return "5.4"
-            case .v5_5:
-                return "5.5"
-            case .v5_6:
-                return "5.6"
-        }
-    }
 }
 
 
