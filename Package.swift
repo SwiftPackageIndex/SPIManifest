ad7553a7f402ae9c98305f6098ee2be1773b00dc
Ahmad Yasser <ahmadyas_ser@icloud.com>
2022-09-11 14:22:42 +0200

    Add Swift DocC Plugin

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/jpsim/Yams.git", "4.0.0"..<"6.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
