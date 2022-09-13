f9e0b9ae18c7056a1ecb9512073012dfe814ebd0
Sven A. Schmidt <sas@finestructure.co>
2022-09-13 08:20:09 +0200

    Remove Swift 5.3

--- Sources/SPIManifest/SwiftVersion.swift
+++ Sources/SPIManifest/SwiftVersion.swift
@@ -16,7 +16,6 @@ public typealias ShortVersion = String
 
 
 public enum SwiftVersion: ShortVersion, Codable {
-    case v5_3 = "5.3"
     case v5_4 = "5.4"
     case v5_5 = "5.5"
     case v5_6 = "5.6"
